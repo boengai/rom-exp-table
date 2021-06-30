d23c7b160d8fb6021f7bac7b108d1d7a27aca0cc
Boe Khattiya <keerati.khat@gmail.com>
2021-06-30 23:37:58 +0700

    👷: print env

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Running on {process.env.NODE_ENV} environment.</p>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
